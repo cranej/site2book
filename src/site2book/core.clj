308e4448db73cdddeb799eaad6b6ddf0a55a9d22
Crane Jin <crane.jin.l@gmail.com>
2014-08-27 13:40:52 +0800

    first commit

---
+++ src/site2book/core.clj
@@ -0,0 +1,6 @@
+(ns site2book.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/site2book/core_test.clj
@@ -0,0 +1,7 @@
+(ns site2book.core-test
+  (:require [clojure.test :refer :all]
+            [site2book.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
