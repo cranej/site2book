bf9e062141e43228bc75a80fe4b3c37b4a605bb2
Crane Jin <crane.jin.l@gmail.com>
2014-08-28 09:24:53 +0800

    parse command line

--- src/site2book/core.clj
+++ src/site2book/core.clj
@@ -1,6 +1,16 @@
-(ns site2book.core)
+(ns site2book.core
+  (:require [net.cgrand.enlive-html :as enlive])
+  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def cli-options
+  [["-u" "--url URL" "URL of the table of content page, multiple URLs splited by '|'."]
+   ["-i" "--item-selector SELECTOR" "CSS selector to select item link in TOC page."
+    :default ""]
+   ["-t" "--title-selector SELECTOR" "CSS selector to select title from each item page."]
+   ["-c" "--content-selector SELECTOR" "CSS selector to select content from each item page."]])
+
+(defn -main [& args]
+  (let [{{:keys [url item-selector title-selector content-selector]} :options} (parse-opts args cli-options)]
+    (print url item-selector)))
+
